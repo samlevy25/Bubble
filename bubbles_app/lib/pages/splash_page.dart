b906b5ec115ff0af1014547dcee1d389d2100a25
Adane-Adgo <111367263+Adane-Adgo@users.noreply.github.com>
2023-02-06 22:48:15 +0200

    Splash Page + Navigator

---
+++ bubbles_app/lib/pages/splash_page.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+//Packegs
+import 'package:firebase_core/firebase_core.dart';
+import 'package:get_it/get_it.dart';
+
+//Pages
+import '../services/navigation_server.dart';
+
+class SplashPage extends StatefulWidget {
+  final VoidCallback onInitializationComplete;
+
+  const SplashPage({
+    required key,
+    required this.onInitializationComplete,
+  }) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _SplashPageState();
+  }
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    _setup().then(
+      (_) => widget.onInitializationComplete(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Bubbles',
+      theme: ThemeData(),
+      home: Scaffold(
+        body: Center(
+          child: Container(
+            height: 200,
+            width: 200,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.contain,
+                image: AssetImage('assets/images/b.jpeg'),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> _setup() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp();
+    _registerServices();
+  }
+}
+
+void _registerServices() {
+  GetIt.instance.registerSingleton<NavigationService>(
+    NavigationService(),
+  );
+}
