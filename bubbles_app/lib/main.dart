b2f19ed696b128bf24cae48c9b7ff93ef44b607d
Adane-Adgo <111367263+Adane-Adgo@users.noreply.github.com>
2023-02-07 21:10:55 +0200

    implment auth provider

--- bubbles_app/lib/main.dart
+++ bubbles_app/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 // Packages
 import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:provider/provider.dart';
 
 //Pages
 import './pages/splash_page.dart';
@@ -9,6 +10,9 @@ import './pages/login_page.dart';
 
 //Servies
 import './services/navigation_server.dart';
+
+//Provider
+import './providers/authentication_provider.dart';
 
 void main() {
   runApp(
@@ -26,18 +30,27 @@ void main() {
 class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Bubbles',
-      theme: ThemeData(
-        scaffoldBackgroundColor: Colors.red,
-        bottomNavigationBarTheme:
-            const BottomNavigationBarThemeData(backgroundColor: Colors.yellow),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<AuthenticationProvider>(
+          create: (BuildContext _context) {
+            return AuthenticationProvider();
+          },
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Bubbles',
+        theme: ThemeData(
+          scaffoldBackgroundColor: Colors.red,
+          bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+              backgroundColor: Colors.yellow),
+        ),
+        navigatorKey: NavigationService.navigatorKey,
+        initialRoute: '/login',
+        routes: {
+          '/login': (BuildContext _context) => LoginPage(),
+        },
       ),
-      navigatorKey: NavigationService.navigatorKey,
-      initialRoute: '/login',
-      routes: {
-        '/login': (BuildContext _context) => LoginPage(),
-      },
     );
   }
 }
---
+++ bubbles_app/lib/providers/authentication_provider.dart
@@ -0,0 +1,20 @@
+//p
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get_it/get_it.dart';
+
+//services
+import '../services/database_service.dart';
+import '../services/navigation_server.dart';
+
+class AuthenticationProvider extends ChangeNotifier {
+  late final FirebaseAuth _auth;
+  late final NavigationService _navigationService;
+  late final DatabaseService _databaseService;
+
+  AuthenticationProvider() {
+    _auth = FirebaseAuth.instance;
+    _navigationService = GetIt.instance.get<NavigationService>();
+    _databaseService = GetIt.instance.get<DatabaseService>();
+  }
+}
