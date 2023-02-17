8faa02c0976400995b5ddc57170b15b24a4436e7
Adane-Adgo <111367263+Adane-Adgo@users.noreply.github.com>
2023-02-17 16:41:32 +0200

    naviagation bar

--- bubbles_app/lib/pages/home_page.dart
+++ bubbles_app/lib/pages/home_page.dart
@@ -8,12 +8,40 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int currentPage = 0;
+  final List<Widget> _pages = [
+    Container(
+      color: Colors.red,
+    ),
+    Container(
+      color: Colors.green,
+    ),
+    Container(
+      color: Colors.blue,
+    )
+  ];
+
   @override
   Widget build(BuildContext context) {
     return _buildUI();
   }
 
   Widget _buildUI() {
-    return Scaffold();
+    return Scaffold(
+      body: _pages[currentPage],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentPage,
+        onTap: (_index) {
+          setState(() {
+            currentPage = _index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(label: "A", icon: Icon(Icons.abc)),
+          BottomNavigationBarItem(label: "B", icon: Icon(Icons.abc)),
+          BottomNavigationBarItem(label: "C", icon: Icon(Icons.abc)),
+        ],
+      ),
+    );
   }
 }
