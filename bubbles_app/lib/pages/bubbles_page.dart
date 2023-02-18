9c35e68b26d0a1df4b8135c1ec250ffb733d963c
Adane-Adgo <111367263+Adane-Adgo@users.noreply.github.com>
2023-02-18 17:00:30 +0200

    top bar

--- bubbles_app/lib/pages/bubbles_page.dart
+++ bubbles_app/lib/pages/bubbles_page.dart
@@ -2,6 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:get_it/get_it.dart';
 
+//p
+import '../providers/authentication_provider.dart';
+
+//w
+import '../widgets/top_bar.dart';
+
 class BubblesPage extends StatefulWidget {
   const BubblesPage({super.key});
 
@@ -10,12 +16,44 @@ class BubblesPage extends StatefulWidget {
 }
 
 class _BubblesPageState extends State<BubblesPage> {
+  late double _deviceHeight;
+  late double _deviceWidth;
+  late AuthenticationProvider _auth;
   @override
   Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    _auth = Provider.of<AuthenticationProvider>(context);
     return _buildUI();
   }
 
   Widget _buildUI() {
-    return Scaffold(backgroundColor: Colors.purple);
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: _deviceWidth * 0.03,
+        vertical: _deviceHeight * 0.02,
+      ),
+      height: _deviceHeight * 0.98,
+      width: _deviceWidth * 0.97,
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          TopBar(
+            'Bubbles',
+            primaryAction: IconButton(
+              icon: const Icon(
+                Icons.logout,
+                color: Color.fromRGBO(0, 82, 218, 1.0),
+              ),
+              onPressed: () {
+                _auth.logout();
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
