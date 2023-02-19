a229da9e10ee5163f2916a65ea1dc0ec069a66a0
Adane-Adgo <111367263+Adane-Adgo@users.noreply.github.com>
2023-02-19 15:14:23 +0200

    extend db service functions for char page

--- bubbles_app/lib/services/database_service.dart
+++ bubbles_app/lib/services/database_service.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 
+import '../models/chat_message.dart';
+
 const String userCollection = "Users";
 const String chatsCollection = "Chats";
 const String messagesCollection = "Messages";
@@ -45,6 +47,42 @@ class DatabaseService {
         .get();
   }
 
+  Stream<QuerySnapshot> streamMessagesForChat(String _chatID) {
+    return _db
+        .collection(chatsCollection)
+        .doc(_chatID)
+        .collection(messagesCollection)
+        .orderBy("sent_time", descending: false)
+        .snapshots();
+  }
+
+  Future<void> updateChatData(
+      String _chatID, Map<String, dynamic> _data) async {
+    try {
+      await _db.collection(chatsCollection).doc(_chatID).update(_data);
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+    }
+  }
+
+  Future<void> addMessageToChat(String _chatID, ChatMessage _message) async {
+    try {
+      await _db
+          .collection(chatsCollection)
+          .doc(_chatID)
+          .collection(messagesCollection)
+          .add(
+            _message.toJson(),
+          );
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+    }
+  }
+
   Future<DocumentSnapshot> getUser(String uid) {
     return _db.collection(userCollection).doc(uid).get();
   }
@@ -56,6 +94,16 @@ class DatabaseService {
           "last_active": DateTime.now().toUtc(),
         },
       );
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+    }
+  }
+
+  Future<void> deleteChat(String _chatID) async {
+    try {
+      await _db.collection(chatsCollection).doc(_chatID).delete();
     } catch (e) {
       if (kDebugMode) {
         print(e);
