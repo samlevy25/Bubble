d82198dffb68011ff2f816baf6a298dc54cb5926
Adane-Adgo <111367263+Adane-Adgo@users.noreply.github.com>
2023-02-19 14:53:36 +0200

    chat page provider

--- bubbles_app/lib/pages/chat_page.dart
+++ bubbles_app/lib/pages/chat_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 //Widgets
+import '../providers/chat_page_provider.dart';
 import '../widgets/top_bar.dart';
 import '../widgets/custom_list_view_tiles.dart';
 import '../widgets/custom_input_fields.dart';
@@ -30,53 +31,85 @@ class _ChatPageState extends State<ChatPage> {
   late double _deviceWidth;
 
   late AuthenticationProvider _auth;
+  late ChatPageProvider _pageProvider;
 
   late GlobalKey<FormState> _messageFormState;
   late ScrollController _messagesListViewController;
 
   @override
+  void initState() {
+    super.initState();
+    _messageFormState = GlobalKey<FormState>();
+    _messagesListViewController = ScrollController();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return buildUI();
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    _auth = Provider.of<AuthenticationProvider>(context);
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<ChatPageProvider>(
+          create: (_) => ChatPageProvider(
+            widget.chat.uid,
+            _auth,
+            _messagesListViewController,
+          ),
+        ),
+      ],
+      child: buildUI(),
+    );
   }
 
   Widget buildUI() {
-    _deviceHeight = MediaQuery.of(context).size.height;
-    _deviceWidth = MediaQuery.of(context).size.width;
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
-          padding: EdgeInsets.symmetric(
-            horizontal: _deviceWidth * 0.03,
-            vertical: _deviceHeight * 0.02,
+    return Builder(
+      builder: (BuildContext context) {
+        _pageProvider = context.watch<ChatPageProvider>();
+        return Scaffold(
+          body: SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.symmetric(
+                horizontal: _deviceWidth * 0.03,
+                vertical: _deviceHeight * 0.02,
+              ),
+              height: _deviceHeight,
+              width: _deviceWidth * 0.97,
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  TopBar(
+                    widget.chat.title(),
+                    fontSize: 10,
+                    primaryAction: IconButton(
+                      icon: const Icon(
+                        Icons.delete,
+                        color: Color.fromRGBO(0, 82, 218, 1.0),
+                      ),
+                      onPressed: () {
+                        // _pageProvider.deleteChat();
+                      },
+                    ),
+                    secondaryAction: IconButton(
+                      icon: const Icon(
+                        Icons.arrow_back,
+                        color: Color.fromRGBO(0, 82, 218, 1.0),
+                      ),
+                      onPressed: () {
+                        _pageProvider.goBack();
+                      },
+                    ),
+                  ),
+                  // _messagesListView(),
+                  //_sendMessageForm(),
+                ],
+              ),
+            ),
           ),
-          height: _deviceHeight,
-          width: _deviceWidth * 0.97,
-          child: Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                TopBar(
-                  widget.chat.title(),
-                  fontSize: 20,
-                  primaryAction: IconButton(
-                    icon: const Icon(
-                      Icons.delete,
-                      color: Color.fromRGBO(0, 82, 218, 1.0),
-                    ),
-                    onPressed: () {},
-                  ),
-                  secondaryAction: IconButton(
-                    icon: const Icon(
-                      Icons.arrow_back,
-                      color: Color.fromRGBO(0, 82, 218, 1.0),
-                    ),
-                    onPressed: () {},
-                  ),
-                )
-              ]),
-        ),
-      ),
+        );
+      },
     );
   }
 }
--- bubbles_app/lib/providers/chat_page_provider.dart
+++ bubbles_app/lib/providers/chat_page_provider.dart
@@ -17,3 +17,46 @@ import '../providers/authentication_provider.dart';
 
 //Models
 import '../models/chat_message.dart';
+
+class ChatPageProvider extends ChangeNotifier {
+  late DatabaseService _db;
+  late CloudStorageService _storage;
+  late MediaService _media;
+  late NavigationService _navigation;
+
+  AuthenticationProvider _auth;
+  ScrollController _messagesListViewController;
+
+  String _chatId;
+  List<ChatMessage>? messages;
+
+  //late StreamSubscription _messagesStream;
+  //late StreamSubscription _keyboardVisibilityStream;
+  //late KeyboardVisibilityController _keyboardVisibilityController;
+
+  String? _message;
+
+  String get message {
+    return message;
+  }
+
+  ChatPageProvider(this._chatId, this._auth, this._messagesListViewController) {
+    _db = GetIt.instance.get<DatabaseService>();
+    _storage = GetIt.instance.get<CloudStorageService>();
+    _media = GetIt.instance.get<MediaService>();
+    _navigation = GetIt.instance.get<NavigationService>();
+    //_keyboardVisibilityController = KeyboardVisibilityController();
+    //listenToMessages();
+    //listenToKeyboardChanges();
+  }
+
+  @override
+  void dispose() {
+    //_messagesStream.cancel();
+    super.dispose();
+  }
+
+  void goBack() {
+    _navigation.goBack();
+  }
+}
